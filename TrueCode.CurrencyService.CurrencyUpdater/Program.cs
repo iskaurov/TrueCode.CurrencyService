9703d6db02da942dc086256a033bbab314cfbd55
ILYA KAUROV <iskaurov@gmail.com>
2025-03-28 22:06:03 +0900

    CurrencyUpdater to Docker

--- TrueCode.CurrencyService.CurrencyUpdater/Program.cs
+++ TrueCode.CurrencyService.CurrencyUpdater/Program.cs
@@ -1,20 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using TrueCode.CurrencyService.CurrencyUpdater;
 using TrueCode.CurrencyService.Infrastructure.Db;
+using TrueCode.CurrencyService.Infrastructure.Common;
 
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+var envName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+var envFile = envName == "Development" ? ".env.dev" : ".env";
+
+// Загружаем переменные из .env (лежит уровнем выше)
+EnvLoader.Load("../", envFile);
 
 Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration(config =>
     {
-        config.AddJsonFile("appsettings.json", optional: false);
+        config
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables();
     })
     .ConfigureServices((context, services) =>
     {
         services.AddHttpClient();
 
         services.AddDbContext<CurrencyDbContext>(options =>
-            options.UseNpgsql(context.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(context.Configuration["DEFAULT_CONNECTION"]));
 
         services.Configure<CurrencyUpdaterOptions>(
             context.Configuration.GetSection("CurrencyUpdater"));
